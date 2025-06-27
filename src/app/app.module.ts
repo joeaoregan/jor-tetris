f3d2c8d4e619c0d68354c93e4ab347eaba018ff6
joeaoregan <joe.oregan@gmail.com>
2025-06-27 18:03:27 +0100

    update dependencies, server, etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
-import { InfoComponent } from './info/info.component';
 import { FooterComponent } from './footer/footer.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [AppComponent, BoardComponent, InfoComponent, FooterComponent],
